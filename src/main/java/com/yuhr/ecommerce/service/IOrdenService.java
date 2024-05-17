7d337393e094b93b631563a4d9fc85f4bd71dd35
Yeisson <lyeisson24@gmail.com>
2024-05-17 13:35:34 -0600

    Metodo para generar numero de Orden

--- src/main/java/com/yuhr/ecommerce/service/IOrdenService.java
+++ src/main/java/com/yuhr/ecommerce/service/IOrdenService.java
@@ -1,9 +1,12 @@
 package com.yuhr.ecommerce.service;
 
+import java.util.List;
+
 import com.yuhr.ecommerce.model.Orden;
 
 public interface IOrdenService {
 
+    List<Orden> findAll();
     Orden save(Orden orden);
 
 }
