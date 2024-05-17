c887762adbd2d3467553a22c344b3b7414e54a01
Yeisson <lyeisson24@gmail.com>
2024-05-17 10:22:02 -0600

    Validacion para no duplicar productos en el carrito y vitsta carrito en el header del usuario

--- src/main/java/com/yuhr/ecommerce/controller/HomeController.java
+++ src/main/java/com/yuhr/ecommerce/controller/HomeController.java
@@ -74,7 +74,15 @@ public class HomeController {
         detalleOrden.setTotal(producto.getPrecio() * cantidad);
         detalleOrden.setProducto(producto);
 
-        detalles.add(detalleOrden);
+        //validad que el producto no se agregue 2 veces
+        Integer idProducto = producto.getId();
+        boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId() == idProducto);
+
+        if (!ingresado) {
+            detalles.add(detalleOrden);
+        }
+
+
 
         sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
 
@@ -112,6 +120,16 @@ public class HomeController {
 
         return "usuario/carrito";
     }
+
+    @GetMapping("/getCart")
+    public String getCart(Model model) {
+
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
+
+        return "usuario/carrito";
+    }
+    
     
 
 }
