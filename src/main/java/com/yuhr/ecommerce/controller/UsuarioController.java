d9f16bea8bfcdf665dc7bb676c205da979bafda3
Yeisson <lyeisson24@gmail.com>
2024-05-20 16:55:09 -0600

    mostrar la vista de compras para el user

--- src/main/java/com/yuhr/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/yuhr/ecommerce/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.yuhr.ecommerce.model.Usuario;
@@ -15,6 +16,8 @@ import jakarta.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -67,6 +70,15 @@ public class UsuarioController {
 
         return "redirect:/";
     }
+
+    @GetMapping("/compras")
+    public String obtenerCompras(Model model, HttpSession session) {
+
+        model.addAttribute("session", session.getAttribute("idusuario"));
+
+        return "/usuario/compras";
+    }
+    
     
     
     
